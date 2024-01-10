4cc5d50fcf0b76c7f6fa6cebde2b6e091c5bbccf
tony <tony@u1s1.biz>
2024-01-10 16:57:29 +0400

    t

--- src/views/main/home/index.tsx
+++ src/views/main/home/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button } from '@arco-design/mobile-react';
 import { useNavigate } from "react-router";
 
-//////
+////// 123
 const Home = ()=>{
     const navigate = useNavigate()
     const handleOnclick=()=>{
