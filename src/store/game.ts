b8be9713aa47c47b3d5e05ffbbdb91d6983d75be
DESKTOP-0LS1FE0\admin <jokerma@gmail.com>
2024-01-11 19:05:01 +0800

    使用react-hook-simple-state 状态

--- src/store/game.ts
+++ src/store/game.ts
@@ -1,4 +1,5 @@
-import { useStore } from './base/useStore';
+import { useStore } from "react-hook-simple-state";
+
 
 export const useStoreCount = (defaultValue?: number):[number, (data: number) => void] => {
   return useStore<number>('storeCount', defaultValue);
